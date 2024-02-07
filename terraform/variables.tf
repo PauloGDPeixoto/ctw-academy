ba8c36f809e29e064e6d7a17aba453d9ed2ab894
Paulo Peixoto <Paulo.Peixoto@ctw.bmwgroup.com>
2024-02-07 15:25:40 +0000

    update config

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,16 +1,18 @@
 variable "resource_group_name" {
-  default     = "cicd-academy-rg"
+  type        = string
+  default     = "cybersecurity-rg"
   description = "Resource group name."
 }
 
 variable "resource_group_location" {
+  type        = string
   default     = "germanywestcentral" # change to "westeurope", if azure region quota limit has been exceeded
   description = "Location of the resource group."
 }
 
 variable "azure_subscription_id" {
   type        = string
-  default     = "470bd2ee-0973-4f49-a719-dc721d0d6e4f"
+  default     = "2d5ebd87-43d2-4406-8625-4fa518b48227"
   description = "Azure Account"
 }
 
@@ -19,4 +21,3 @@ variable "arm_tenant_id" {
   description = "Azure tenant id"
   default     = "61f30b8e-4f6b-44fe-9bc2-041e3a9f7346"
 }
-
