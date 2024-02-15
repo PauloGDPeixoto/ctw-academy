cb6602ff0129c04c58adc79b1fb013ac6a7c6c3d
Paulo Peixoto <Paulo.Peixoto@ctw.bmwgroup.com>
2024-02-15 14:51:28 +0000

    add full ansible and terraform code

--- terraform/main.tf
+++ terraform/main.tf
@@ -14,3 +14,115 @@ resource "azurerm_virtual_network" "myterraformnetwork" {
   location            = var.resource_group_location
   resource_group_name = var.resource_group_name
 }
+
+# Create subnet
+resource "azurerm_subnet" "myterraformsubnet" {
+  name                 = "subnet${random_string.number.result}"
+  resource_group_name  = var.resource_group_name
+  virtual_network_name = azurerm_virtual_network.myterraformnetwork.name
+  address_prefixes     = ["10.0.1.0/24"]
+}
+
+
+# Create public IPs
+resource "azurerm_public_ip" "myterraformpublicip" {
+  name                = "publicIP${random_string.number.result}"
+  location            = var.resource_group_location
+  resource_group_name = var.resource_group_name
+  allocation_method   = "Dynamic"
+  domain_name_label   = "ctwacademy${random_string.number.result}"
+}
+
+# Create Network Security Group and rule
+resource "azurerm_network_security_group" "myterraformnsg" {
+  name                = "networkSecurityGroup${random_string.number.result}"
+  location            = var.resource_group_location
+  resource_group_name = var.resource_group_name
+
+  security_rule {
+    name                       = "SSH"
+    priority                   = 1001
+    direction                  = "Inbound"
+    access                     = "Allow"
+    protocol                   = "Tcp"
+    source_port_range          = "*"
+    destination_port_range     = "22"
+    source_address_prefix      = "*"
+    destination_address_prefix = "*"
+  }
+
+  security_rule {
+    name                       = "NodeExporter"
+    priority                   = 1002
+    direction                  = "Inbound"
+    access                     = "Allow"
+    protocol                   = "Tcp"
+    source_port_range          = "*"
+    destination_port_range     = "9100"
+    source_address_prefix      = "*"
+    destination_address_prefix = "*"
+  }
+
+  security_rule {
+    name                       = "http"
+    priority                   = 1003
+    direction                  = "Inbound"
+    access                     = "Allow"
+    protocol                   = "Tcp"
+    source_port_range          = "*"
+    destination_port_range     = "80"
+    source_address_prefix      = "*"
+    destination_address_prefix = "*"
+  }
+}
+
+# Create network interface
+resource "azurerm_network_interface" "myterraformnic" {
+  name                = "NIC${random_string.number.result}"
+  location            = var.resource_group_location
+  resource_group_name = var.resource_group_name
+
+  ip_configuration {
+    name                          = "nicConfiguration${random_string.number.result}"
+    subnet_id                     = azurerm_subnet.myterraformsubnet.id
+    private_ip_address_allocation = "Dynamic"
+    public_ip_address_id          = azurerm_public_ip.myterraformpublicip.id
+  }
+}
+
+# Connect the security group to the network interface
+resource "azurerm_network_interface_security_group_association" "example" {
+  network_interface_id      = azurerm_network_interface.myterraformnic.id
+  network_security_group_id = azurerm_network_security_group.myterraformnsg.id
+}
+
+# Create virtual machine
+resource "azurerm_linux_virtual_machine" "myterraformvm" {
+  name                  = "vm${random_string.number.result}"
+  location              = var.resource_group_location
+  resource_group_name   = var.resource_group_name
+  network_interface_ids = [azurerm_network_interface.myterraformnic.id]
+  size                  = "Standard_A1_v2"
+
+  os_disk {
+    name                 = "osDisk${random_string.number.result}"
+    caching              = "ReadWrite"
+    storage_account_type = "Standard_LRS"
+  }
+
+  source_image_reference {
+    publisher = "Canonical"
+    offer     = "UbuntuServer"
+    sku       = "18.04-LTS"
+    version   = "latest"
+  }
+
+  computer_name                   = "vm${random_string.number.result}"
+  admin_username                  = "ctwacademy"
+  disable_password_authentication = true
+
+  admin_ssh_key {
+    username   = "ctwacademy"
+    public_key = file("../resources/id_rsa.pub")
+  }
+}
